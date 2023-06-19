6eb1a763e46a3da15d017e6d4928f93fccc24de4
Faqih Muntashir <itsfaqih@gmail.com>
2023-06-19 13:38:51 +0700

    feat: add vercel analytics

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,5 +1,6 @@
 import "./globals.css";
 import { Inter } from "next/font/google";
+import { Analytics } from "@vercel/analytics/react";
 
 const inter = Inter({ subsets: ["latin"] });
 
@@ -20,6 +21,7 @@ export default function RootLayout({ children, modal }: RootLayoutProps) {
       <body className={inter.className}>
         {children}
         {modal}
+        <Analytics />
       </body>
     </html>
   );
